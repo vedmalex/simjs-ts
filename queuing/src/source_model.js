56fdb81fc1613870eb4540ccc291cdd387812378
Bernardo Telles <btelles@google.com>
2016-05-06 20:31:34 -0700

    Update queuing code to remove tabs and fix lint.

--- queuing/src/source_model.js
+++ queuing/src/source_model.js
@@ -1,66 +1,66 @@
 function SourceModel(view) {
-	this.view = view;
-	this.lambda = 0.25;
-	this.dest = null;
-	this.view.image.attr({title: 'Interarrival rate = ' + this.lambda});
+  this.view = view;
+  this.lambda = 0.25;
+  this.dest = null;
+  this.view.image.attr({title: 'Interarrival rate = ' + this.lambda});
 }
 
 SourceModel.prototype.jsonify = function () {
-	return {
-		lambda: this.lambda
-	};
+  return {
+    lambda: this.lambda
+  };
 };
 
 SourceModel.prototype.start = function () {
-	this.entity = QueueApp.sim.addEntity(SourceEntity, this.lambda);
+  this.entity = QueueApp.sim.addEntity(SourceEntity, this.lambda);
 };
 
 SourceModel.prototype.connect = function () {
-	this.entity.dest = this.dest ? this.dest.entity : null;
+  this.entity.dest = this.dest ? this.dest.entity : null;
 };
 
 SourceModel.prototype.showSettings = function () {
-	var d = $('#source_form');
-	QueueApp.form_view = this.view;
-	d.find('#source_form_rate').val(this.lambda);
-	
-	d.show().position({
-		of: $(this.view.image.node),
-		at: 'center center',
-		my: 'left top'
-	});
+  var d = $('#source_form');
+  QueueApp.form_view = this.view;
+  d.find('#source_form_rate').val(this.lambda);
+
+  d.show().position({
+    of: $(this.view.image.node),
+    at: 'center center',
+    my: 'left top'
+  });
 };
 
 SourceModel.prototype.saveSettings = function (dialog) {
-	var d = $('#source_form');
-	this.lambda = d.find('#source_form_rate').val();
-	this.view.image.attr({title: 'Interarrival rate = ' + this.lambda});
+  var d = $('#source_form');
+  this.lambda = d.find('#source_form_rate').val();
+  this.view.image.attr({title: 'Interarrival rate = ' + this.lambda});
 };
 
 SourceModel.prototype.unlink = function () {
-	this.view = null;
+  this.view = null;
 };
 
 SourceModel.prototype.showStats = function () {
-	this.view.showCounters(NaN, this.entity.generated);
+  this.view.showCounters(NaN, this.entity.generated);
 };
 
 /*-------------------------*/
 var SourceEntity = {
-	start: function (lambda) {
-		this.lambda = lambda;
-		this.setTimer(0).done(this.traffic);
-		this.generated = 0;
-	},
-	
-	traffic: function () {
-		if (!this.dest) return;
-		this.dest.arrive(this.time());
+  start: function (lambda) {
+    this.lambda = lambda;
+    this.setTimer(0).done(this.traffic);
+    this.generated = 0;
+  },
 
-		this.generated ++;
-		
-		var duration = QueueApp.random.exponential(this.lambda);
+  traffic: function () {
+    if (!this.dest) return;
+    this.dest.arrive(this.time());
 
-		this.setTimer(duration).done(this.traffic);
-	}
+    this.generated ++;
+
+    var duration = QueueApp.random.exponential(this.lambda);
+
+    this.setTimer(duration).done(this.traffic);
+  }
 };
